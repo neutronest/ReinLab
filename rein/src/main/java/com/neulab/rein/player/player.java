df489d7aaf78c24ed5cf3608bbc0c9be41dad3eb
neutronest <neutronest@gmail.com>
2018-11-05 22:17:38 +0800

    @neutronest Dev: add player

---
+++ rein/src/main/java/com/neulab/rein/player/player.java
@@ -0,0 +1,44 @@
+package com.neulab.rein.player;
+
+public class player {
+
+    public Double baseHP;
+    public Double baseEP;
+    public Double baseSP;
+
+    public Double getBaseHP() {
+        return baseHP;
+    }
+
+    public void setBaseHP(Double baseHP) {
+        this.baseHP = baseHP;
+    }
+
+
+    public Double getBaseEP() {
+        return baseEP;
+    }
+
+    public void setBaseEP(Double baseEP) {
+        this.baseEP = baseEP;
+    }
+
+    public Double getBaseSP() {
+        return baseSP;
+    }
+
+    public void setBaseSP(Double baseSP) {
+        this.baseSP = baseSP;
+    }
+
+
+
+
+//    double getBaseHP();
+//
+//    double getBaseEP();
+//
+//    double getBaseSP();
+//
+//    double getBaseATK();
+}
