aba5711c998f248fe75dd09dc8d3d0d96eafcc25
neutronest <neutronest@gmail.com>
2018-11-06 00:35:00 +0800

    @neutronest Dev: basic attributes for player

--- rein/src/main/java/com/neulab/rein/player/player.java
+++ rein/src/main/java/com/neulab/rein/player/player.java
@@ -1,10 +1,80 @@
 package com.neulab.rein.player;
 
-public class player {
+public class Player {
 
     public Double baseHP;
     public Double baseEP;
     public Double baseSP;
+
+    public Double curHP;
+    public Double curEP;
+    public Double curSP;
+
+
+    public Double baseATK;
+    public Double curATK;
+
+    public Integer shell;
+
+    public Integer getEncourage() {
+        return encourage;
+    }
+
+    public void setEncourage(Integer encourage) {
+        this.encourage = encourage;
+    }
+
+    public Integer encourage;
+
+    public Double getBaseATK() {
+        return baseATK;
+    }
+
+    public void setBaseATK(Double baseATK) {
+        this.baseATK = baseATK;
+    }
+
+    public Double getCurATK() {
+        return curATK;
+    }
+
+    public void setCurATK(Double curATK) {
+        this.curATK = curATK;
+    }
+
+
+    public Double getCurHP() {
+        return curHP;
+    }
+
+    public void setCurHP(Double curHP) {
+        this.curHP = curHP;
+    }
+
+    public Double getCurEP() {
+        return curEP;
+    }
+
+    public void setCurEP(Double curEP) {
+        this.curEP = curEP;
+    }
+
+    public Double getCurSP() {
+        return curSP;
+    }
+
+    public void setCurSP(Double curSP) {
+        this.curSP = curSP;
+    }
+
+    public Integer getShell() {
+        return shell;
+    }
+
+    public void setShell(Integer shell) {
+        this.shell = shell;
+    }
+
 
     public Double getBaseHP() {
         return baseHP;
